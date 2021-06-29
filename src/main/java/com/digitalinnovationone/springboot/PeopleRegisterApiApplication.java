5bdff205f59eeec8a79bebefffeab71ca3fa9666
paleblueweb <paleblue.web@outlook.com>
2021-06-29 14:39:52 -0300

    first commit

---
+++ src/main/java/com/digitalinnovationone/springboot/PeopleRegisterApiApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleRegisterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleRegisterApiApplication.class, args);
+	}
+
+}
