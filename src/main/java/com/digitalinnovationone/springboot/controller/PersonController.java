6c8634475d7bb110f072ce214b45b2d3f8212506
rabbithatt <rabbithat.red@gmail.com>
2021-06-30 17:47:18 -0300

    readme file

--- src/main/java/com/digitalinnovationone/springboot/controller/PersonController.java
+++ src/main/java/com/digitalinnovationone/springboot/controller/PersonController.java
@@ -6,6 +6,7 @@ import com.digitalinnovationone.springboot.entity.Person;
 import com.digitalinnovationone.springboot.exception.PersonNotFoundException;
 import com.digitalinnovationone.springboot.repository.PersonRepository;
 import com.digitalinnovationone.springboot.service.PersonService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,14 +16,11 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
     private PersonService personService;
 
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
